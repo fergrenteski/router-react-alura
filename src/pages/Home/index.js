7c0e69487ab26a7dd730c31d47b1d488e2b37276
Luiz Fernando Grenteski <luiz.grenteski@rrs-software.com.br>
2025-02-20 12:36:30 -0300

    :sparkles: Feat: Posts.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Banner from 'components/Banner'
+import styles from './Home.module.css'
+import posts from 'json/posts.json'
+import Post from 'components/Post'
 
 const home = () => {
   return (
     <main>
       <Banner />
-      <div>home</div>
+      <ul className={styles.posts}>
+      {posts.map((post) => (
+          <li key={post.id}>
+            <Post post={post} />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
