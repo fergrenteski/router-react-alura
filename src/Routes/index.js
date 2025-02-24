9f4fa629f422b33b8a067aab049afbd1a15741dc
Luiz Fernando Grenteski <luiz.grenteski@rrs-software.com.br>
2025-02-24 09:46:50 -0300

    :sparkles: Post Recomentados e ScrollToTop.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,17 +7,19 @@ import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 import DefaultPage from 'components/common/DefaultPage';
 import Post from 'pages/Post';
+import ScrollToTop from 'components/ScrollToTop';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
      < Header />
       <Routes>
         <Route  path='/' element={<DefaultPage />}>
           <Route index element={<Home/>} />
           <Route path="/about" element={<About/>} />
-          <Route path="/post/:id" element={<Post/>} />
         </Route>
+        <Route path="/post/:id" element={<Post/>} />
         <Route path='*' element={< NotFound />} />
       </Routes>
       <Footer />
--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,28 +1,59 @@
-import './Post.css';
+import "./Post.css";
+import styles from "./Post.module.css";
 
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import posts from 'json/posts.json';
-import PostModel from 'components/PostModel';
-import ReactMarkdown from 'react-markdown';
+import React from "react";
+import { Route, Routes, useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModel from "components/PostModel";
+import ReactMarkdown from "react-markdown";
+import NotFound from "pages/NotFound";
+import DefaultPage from "components/common/DefaultPage";
+import PostCard from "components/PostCard";
 
 const Post = () => {
   const parametros = useParams();
 
   const post = posts.find((post) => post.id === Number(parametros.id));
 
-  if(!post) {
-    return <h1>Post não encontrado...</h1>
+  const postsRecomendados = posts
+    .filter((post) => post.id !== Number(parametros.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0,4);
+
+  if (!post) {
+    return <NotFound />;
   }
   return (
-    <PostModel fotoCapa={`assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
-      <div className='post-markdown-container'>
-        <ReactMarkdown>
-          {post.texto}
-        </ReactMarkdown>
-      </div>
-    </PostModel>
-  )
-}
+    <Routes>
+      <Route path="*" element={<DefaultPage />}>
+        <Route
+          index
+          element={
+            <PostModel
+              fotoCapa={`assets/posts/${post.id}/capa.png`}
+              titulo={post.titulo}
+            >
+              <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
 
-export default Post
+              <h2 className={styles.tituloOutrosPosts}>Outros posts que você pode gostar:</h2>
+
+              <ul className={styles.postsRecomendados}>
+                {postsRecomendados.map((post) => (
+                  <li key={post.id}>
+                    <PostCard
+                      post={post}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </PostModel>
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Post;
